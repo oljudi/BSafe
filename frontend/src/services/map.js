74f16dda141b430cd30c9884814f48893c10e75b
oljudi <oljudimark@gmail.com>
2020-03-05 09:33:37 -0600

    Conncetion to API-DATA

--- frontend/src/services/map.js
+++ frontend/src/services/map.js
@@ -1,6 +1,5 @@
 import axios from "axios";
-const baseURL =
-  "https://datos.cdmx.gob.mx/api/records/1.0/search/?dataset=carpetas-de-investigacion-pgj-de-la-ciudad-de-mexico&rows=200&facet=ao_hechos&facet=mes_hechos&facet=delito&facet=categoria_delito&facet=fiscalia&facet=alcaldia_hechos&facet=colonia_hechos&facet=ao_inicio&facet=mes_inicio&format=geojson";
+const baseURL = process.env.REACT_APP_DATA;
 
 const service = axios.create({ baseURL });
 
--- frontend/src/services/mapc5.js
+++ frontend/src/services/mapc5.js
@@ -1,6 +1,5 @@
 import axios from "axios";
-const baseURL =
-  "https://datos.cdmx.gob.mx/api/records/1.0/search/?dataset=ubicacion-acceso-gratuito-internet-wifi-c5&rows=200&facet=colonia&facet=alcaldia&facet=boton&facet=altavoz&facet=tipo_de_poste&refine.boton=CON+BOTON&format=geojson";
+const baseURL = process.env.REACT_APP_DATAC5
 
 const service = axios.create({ baseURL });
 
